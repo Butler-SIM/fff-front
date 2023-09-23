dec1861b1fa4cf3ca9eb66a3eab7fc76b32827f5
sim <dhksdn4689@naver.com>
2023-09-23 20:22:24 +0900

    fix free-board writing

--- src/components/board/BoardList.tsx
+++ src/components/board/BoardList.tsx
@@ -41,29 +41,31 @@ export function BoardListData(props: any) {
 
   return (
     <Box>
-          <Flex
-      direction="column"
-      justifyContent="space-between"
-      height="100%" // Adjust as needed
-    >
-      <Box>
-        <Box bg={useColorModeValue("gray.100", "gray.900")}>
-          {props.title ? props.title : ""}
+      <Flex
+        direction="column"
+        justifyContent="space-between"
+        height="100%" // Adjust as needed
+      >
+        <Box>
+          <Box bg={useColorModeValue("gray.100", "gray.900")}>
+            {props.title ? props.title : ""}
+          </Box>
+
+          <Table
+            variant="simple"
+            fontSize={useBreakpointValue({ base: "10px", md: "13px" })}
+            mt={20}
+          >
+            {/* ... */}
+          </Table>
         </Box>
 
-        <Table
-          variant="simple"
-          fontSize={useBreakpointValue({ base: "10px", md: "13px" })}
-          mt={20}
-        >
-          {/* ... */}
-        </Table>
-      </Box>
-
-      <Button alignSelf="flex-end">
-        <ChakraLink as={ReactRouterLink} to="/free/writing">글쓰기</ChakraLink>
-      </Button>
-    </Flex>
+        <Button alignSelf="flex-end">
+          <ChakraLink as={ReactRouterLink} to="/free/writing">
+            글쓰기
+          </ChakraLink>
+        </Button>
+      </Flex>
       <Table
         variant="simple"
         fontSize={useBreakpointValue({ base: "10px", md: "13px" })}
@@ -140,10 +142,7 @@ export default function BoardList() {
 
   return (
     <Container maxW={"5xl"}>
-      <Box
-        mt={150}
-        mb={20}
-      >
+      <Box mt={150} mb={20}>
         <BoardListData title={"Today Best"}></BoardListData>
       </Box>
     </Container>
